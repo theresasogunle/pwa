61c964c273e23b4250c6ebcad80dd9b17146b7fd
Omowunmi <sogunledolapo@gmail.com>
2020-04-27 21:15:14 +0100

    added indexed db

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,124 +1,129 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
-var sharedMomentsArea = document.querySelector('#shared-moments');
+var shareImageButton = document.querySelector('#share-image-button')
+var createPostArea = document.querySelector('#create-post')
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn')
+var sharedMomentsArea = document.querySelector('#shared-moments')
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
+	createPostArea.style.display = 'block'
+	if (deferredPrompt) {
+		deferredPrompt.prompt()
 
-    deferredPrompt.userChoice.then(function(choiceResult) {
-      console.log(choiceResult.outcome);
+		deferredPrompt.userChoice.then(function(choiceResult) {
+			console.log(choiceResult.outcome)
 
-      if (choiceResult.outcome === 'dismissed') {
-        console.log('User cancelled installation');
-      } else {
-        console.log('User added to home screen');
-      }
-    });
+			if (choiceResult.outcome === 'dismissed') {
+				console.log('User cancelled installation')
+			} else {
+				console.log('User added to home screen')
+			}
+		})
 
-    deferredPrompt = null;
-  }
+		deferredPrompt = null
+	}
 
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker.getRegistrations()
-  //     .then(function(registrations) {
-  //       for (var i = 0; i < registrations.length; i++) {
-  //         registrations[i].unregister();
-  //       }
-  //     })
-  // }
+	// if ('serviceWorker' in navigator) {
+	//   navigator.serviceWorker.getRegistrations()
+	//     .then(function(registrations) {
+	//       for (var i = 0; i < registrations.length; i++) {
+	//         registrations[i].unregister();
+	//       }
+	//     })
+	// }
+}
+
+function updateUI(data) {
+	for (var i = 0; i < data.length; i++) {
+		createCard(data[i])
+	}
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+	createPostArea.style.display = 'none'
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener('click', openCreatePostModal)
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal)
 
 // Currently not in use, allows to save assets in cache on demand otherwise
 function onSaveButtonClicked(event) {
-  console.log('clicked');
-  if ('caches' in window) {
-    caches.open('user-requested')
-      .then(function(cache) {
-        cache.add('https://httpbin.org/get');
-        cache.add('/src/images/sf-boat.jpg');
-      });
-  }
+	console.log('clicked')
+	if ('caches' in window) {
+		caches.open('user-requested').then(function(cache) {
+			cache.add('https://httpbin.org/get')
+			cache.add('/src/images/sf-boat.jpg')
+		})
+	}
 }
 
 function clearCards() {
-  while(sharedMomentsArea.hasChildNodes()) {
-    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
-  }
+	while (sharedMomentsArea.hasChildNodes()) {
+		sharedMomentsArea.removeChild(sharedMomentsArea.lastChild)
+	}
 }
 
-function createCard() {
-  var cardWrapper = document.createElement('div');
-  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
-  var cardTitle = document.createElement('div');
-  cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
-  cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.height = '180px';
-  cardWrapper.appendChild(cardTitle);
-  var cardTitleTextElement = document.createElement('h2');
-  cardTitleTextElement.style.color = 'white';
-  cardTitleTextElement.className = 'mdl-card__title-text';
-  cardTitleTextElement.textContent = 'San Francisco Trip';
-  cardTitle.appendChild(cardTitleTextElement);
-  var cardSupportingText = document.createElement('div');
-  cardSupportingText.className = 'mdl-card__supporting-text';
-  cardSupportingText.textContent = 'In San Francisco';
-  cardSupportingText.style.textAlign = 'center';
-  // var cardSaveButton = document.createElement('button');
-  // cardSaveButton.textContent = 'Save';
-  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
-  // cardSupportingText.appendChild(cardSaveButton);
-  cardWrapper.appendChild(cardSupportingText);
-  componentHandler.upgradeElement(cardWrapper);
-  sharedMomentsArea.appendChild(cardWrapper);
+function createCard(data) {
+	var cardWrapper = document.createElement('div')
+	cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp'
+	var cardTitle = document.createElement('div')
+	cardTitle.className = 'mdl-card__title'
+	cardTitle.style.backgroundImage = 'url(' + data.image + ')'
+	cardTitle.style.backgroundSize = 'cover'
+	cardTitle.style.height = '180px'
+	cardWrapper.appendChild(cardTitle)
+	var cardTitleTextElement = document.createElement('h2')
+	cardTitleTextElement.style.color = 'white'
+	cardTitleTextElement.className = 'mdl-card__title-text'
+	cardTitleTextElement.textContent = data.title
+	cardTitle.appendChild(cardTitleTextElement)
+	var cardSupportingText = document.createElement('div')
+	cardSupportingText.className = 'mdl-card__supporting-text'
+	cardSupportingText.textContent = data.location
+	cardSupportingText.style.textAlign = 'center'
+	// var cardSaveButton = document.createElement('button');
+	// cardSaveButton.textContent = 'Save';
+	// cardSaveButton.addEventListener('click', onSaveButtonClicked);
+	// cardSupportingText.appendChild(cardSaveButton);
+	cardWrapper.appendChild(cardSupportingText)
+	componentHandler.upgradeElement(cardWrapper)
+	sharedMomentsArea.appendChild(cardWrapper)
 }
 
-var url = 'https://httpbin.org/post';
-var networkDataReceived = false;
+var url = 'https://principal-sonar-184611.firebaseio.com/posts.json'
+var networkDataReceived = false
 
-fetch(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  },
-  body: JSON.stringify({
-    message: 'Some message'
-  })
-})
-  .then(function(res) {
-    return res.json();
-  })
-  .then(function(data) {
-    networkDataReceived = true;
-    console.log('From web', data);
-    clearCards();
-    createCard();
-  });
+fetch(url)
+	.then(function(res) {
+		return res.json()
+	})
+	.then(function(data) {
+		networkDataReceived = true
+		console.log('From web', data)
+		// clearCards()
+		var dataArray = []
+		for (var key in data) {
+			dataArray.push(data[key])
+		}
+		updateUI(dataArray)
+	})
 
 if ('caches' in window) {
-  caches.match(url)
-    .then(function(response) {
-      if (response) {
-        return response.json();
-      }
-    })
-    .then(function(data) {
-      console.log('From cache', data);
-      if (!networkDataReceived) {
-        clearCards();
-        createCard();
-      }
-    });
+	caches
+		.match(url)
+		.then(function(response) {
+			if (response) {
+				return response.json()
+			}
+		})
+		.then(function(data) {
+			console.log('From cache', data)
+			if (!networkDataReceived) {
+				var dataArray = []
+				for (var key in data) {
+					dataArray.push(data[key])
+				}
+				updateUI(dataArray)
+			
+			}
+		})
 }
